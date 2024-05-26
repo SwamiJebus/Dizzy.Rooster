0ac3d411080dc76afdfd0ade07cdb50d2f0b371f
Stink-Jebb <70412105+Stink-Jebb@users.noreply.github.com>
2024-05-25 23:09:50 -0500

    OLED display hello worlding

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,37 @@
 #include <Arduino.h>
+#include <U8g2lib.h>
+#include <Wire.h>
+
+///////////////////////
+//    Pin defines    //
+///////////////////////
+
+int PinSDA = 26;
+int PinSCL = 27;
+
+////////////////////////
+//    Constructors    //
+////////////////////////
+
+TwoWire I2C = TwoWire(0);
+U8G2_SH1106_128X64_NONAME_F_HW_I2C Display = U8G2_SH1106_128X64_NONAME_F_HW_I2C(U8G2_R0, 0, PinSCL, PinSDA);
 
 // put function declarations here:
 int myFunction(int, int);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup() 
+{
+  I2C.begin(PinSDA, PinSCL, 400000); // ESP32 supports "fast-mode" of between 100-400 kHz
+  Display.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop() 
+{
+  Display.firstPage();
+  do 
+  {
+    Display.setFont(u8g2_font_ncenB14_tr);
+    Display.drawStr(0,20,"Hello World!");
+  }
+  while (Display.nextPage()); 
 }
